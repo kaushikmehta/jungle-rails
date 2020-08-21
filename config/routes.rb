668a4c0814362fc1f1a78331a6d955ab505e6a8e
Kaushik Mehta <kaushikmehta30@gmail.com>
2020-08-21 20:58:18 +0000

    adds about page with controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   root to: 'products#index'
 
+  get '/about' => 'about#show'
+
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
@@ -24,7 +26,7 @@ Rails.application.routes.draw do
   # root 'welcome#index'
 
   # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+    # get 'products/:id' => 'catalog#view'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
