1ac33bf29a497ff3bb5610b1c57ee4a04e46bf71
Kaushik Mehta <kaushikmehta30@gmail.com>
2020-08-22 21:10:49 +0000

    adds users email to order

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,7 @@ class Product < ActiveRecord::Base
   mount_uploader :image, ProductImageUploader
 
   belongs_to :category
+  has_many :line_items
 
   validates :name, presence: true
   validates :price, presence: true
@@ -13,7 +14,7 @@ class Product < ActiveRecord::Base
   def sold_out?
     quantity == 0
   end
-  
+
   def available?
     !sold_out?
   end
